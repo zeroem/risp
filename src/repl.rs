f363e6f9f4210e223b982842e6abbd49ea84de1e
Darrell Hamilton <darrell.noice@gmail.com>
2014-07-21 22:22:04 -0700

    basic echo repl

---
+++ src/repl.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main() {
+    println!("Risp Version 0.1.0-SNAPSHOT");
+    println!("Press Ctrl-C to exit");
+    let mut reader = io::stdin();
+
+    while true {
+        print!("> ");
+        let input = reader.read_line().ok().expect("Failed to read line");
+        print!("{:s}", input);
+    }
+}
